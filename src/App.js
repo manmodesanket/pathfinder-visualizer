6d1b54dc7901a8156db3db5eeee3632b4cbc8cb5
Sanket Manmode <manmodesanket@gmail.com>
2020-05-03 13:51:09 +0530

    Setup done

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/styles.css";
+
+const App = () => {
+  return <div>Hello</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
